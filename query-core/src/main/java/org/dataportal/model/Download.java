11694146db2bd15e5d5b82db246233baa98619a7
Micho Garcia <michogar@gmail.com>
2011-09-30 20:32:06 +0200

    Created Model & added to persistence unit

---
+++ query-core/src/main/java/org/dataportal/model/Download.java
@@ -0,0 +1,80 @@
+package org.dataportal.model;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.List;
+
+
+/**
+ * The persistent class for the download database table.
+ * 
+ */
+@Entity
+@Table(name="download")
+public class Download implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@Column(unique=true, nullable=false)
+	private Integer id;
+
+	@Column(length=2147483647)
+	private String filename;
+
+	@Column(nullable=false)
+	private Timestamp timestamp;
+
+	//bi-directional many-to-one association to User
+    @ManyToOne
+	@JoinColumn(name="user")
+	private User userBean;
+
+	//bi-directional many-to-one association to DownloadItem
+	@OneToMany(mappedBy="download")
+	private List<DownloadItem> downloadItems;
+
+    public Download() {
+    }
+
+	public Integer getId() {
+		return this.id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getFilename() {
+		return this.filename;
+	}
+
+	public void setFilename(String filename) {
+		this.filename = filename;
+	}
+
+	public Timestamp getTimestamp() {
+		return this.timestamp;
+	}
+
+	public void setTimestamp(Timestamp timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public User getUserBean() {
+		return this.userBean;
+	}
+
+	public void setUserBean(User userBean) {
+		this.userBean = userBean;
+	}
+	
+	public List<DownloadItem> getDownloadItems() {
+		return this.downloadItems;
+	}
+
+	public void setDownloadItems(List<DownloadItem> downloadItems) {
+		this.downloadItems = downloadItems;
+	}
+	
+}
---
+++ query-core/src/main/java/org/dataportal/model/Search.java
@@ -0,0 +1,113 @@
+package org.dataportal.model;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.sql.Array;
+import java.sql.Timestamp;
+import java.util.Date;
+
+
+/**
+ * The persistent class for the search database table.
+ * 
+ */
+@Entity
+@Table(name="search")
+public class Search implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@Column(unique=true, nullable=false)
+	private Integer id;
+
+	private Array bboxes;
+
+    @Temporal( TemporalType.DATE)
+	@Column(name="end_date")
+	private Date endDate;
+
+    @Temporal( TemporalType.DATE)
+	@Column(name="start_date")
+	private Date startDate;
+
+	@Column(length=2147483647)
+	private String text;
+
+	private Timestamp timestamp;
+
+	@Column(length=2147483647)
+	private Array variables;
+
+	//bi-directional many-to-one association to User
+    @ManyToOne
+	@JoinColumn(name="user")
+	private User userBean;
+
+    public Search() {
+    }
+
+	public Integer getId() {
+		return this.id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Array getBboxes() {
+		return this.bboxes;
+	}
+
+	public void setBboxes(Array bboxes) {
+		this.bboxes = bboxes;
+	}
+
+	public Date getEndDate() {
+		return this.endDate;
+	}
+
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+
+	public Date getStartDate() {
+		return this.startDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+	public String getText() {
+		return this.text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public Timestamp getTimestamp() {
+		return this.timestamp;
+	}
+
+	public void setTimestamp(Timestamp timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public Array getVariables() {
+		return this.variables;
+	}
+
+	public void setVariables(Array variables) {
+		this.variables = variables;
+	}
+
+	public User getUserBean() {
+		return this.userBean;
+	}
+
+	public void setUserBean(User userBean) {
+		this.userBean = userBean;
+	}
+	
+}
