661c487412a72fdc1c7e7475a6cb490905b77715
michogar <michogar@gmail.com>
2011-12-21 18:07:36 +0100

    Added controllers to DataPortalController class

--- query-core/src/main/java/org/dataportal/DataPortalController.java
+++ query-core/src/main/java/org/dataportal/DataPortalController.java
@@ -6,7 +6,11 @@ package org.dataportal;
 import java.net.MalformedURLException;
 
 import org.apache.log4j.Logger;
+import org.dataportal.controllers.JPADownloadController;
+import org.dataportal.controllers.JPASearchController;
+import org.dataportal.controllers.JPAUserController;
 import org.dataportal.csw.Catalog;
+import org.dataportal.csw.DataPortalNS;
 import org.dataportal.utils.DataPortalException;
 
 /**
@@ -19,7 +23,14 @@ public class DataPortalController implements DataportalCodes {
 
 	protected static Catalog catalogo;
 	protected DataPortalException dtException;
+	protected JPAUserController userJPAController;
+	protected JPADownloadController downloadJPAController;
+	protected JPASearchController searchJPAController;
+	protected DataPortalNS dataPortalCtx = new DataPortalNS();
 
+	/**
+	 * @throws MalformedURLException
+	 */
 	public DataPortalController() throws MalformedURLException {
 
 		String url = Config.get("csw.url");
