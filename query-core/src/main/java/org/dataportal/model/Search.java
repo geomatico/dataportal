6c45fd91cd7a67c1900761e845c9fd4197737e0a
michogar <michogar@gmail.com>
2011-12-22 13:19:48 +0100

    Added GeneratedValue to org.dataportal.model.Search

--- query-core/src/main/java/org/dataportal/model/Search.java
+++ query-core/src/main/java/org/dataportal/model/Search.java
@@ -15,6 +15,7 @@ public class Search implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 
 	private String bboxes;
