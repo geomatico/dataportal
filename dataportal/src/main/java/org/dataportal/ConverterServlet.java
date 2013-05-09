2c306e9782133ebd06abfb59a9ea73d37003b883
Micho Garcia <michogar@gmail.com>
2013-05-09 13:36:24 +0200

    Added ConverterServlet
    Added ConverterServlet and modified pom to manage new dependencies. Fixed problems with the version of compiler in netcdf-converter pom.xml file.

---
+++ dataportal/src/main/java/org/dataportal/ConverterServlet.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package org.dataportal;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author Micho Garcia
+ *
+ */
+public class ConverterServlet extends HttpServlet implements DataportalCodes {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doPost(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		
+	}
+
+	
+}
