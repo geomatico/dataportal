b96323f05d744cbf8273b879b001c84ac7f343ce
Micho Garcia <michogar@gmail.com>
2013-05-20 13:29:54 +0200

    Created some components into Upload Form
    Added Panel for Global metadata, and panel for buttons institution

--- dataportal/src/main/webapp/js/converter.Form.js
+++ dataportal/src/main/webapp/js/converter.Form.js
@@ -2,5 +2,68 @@
  * 
  */
 Ext.define('converter.Form', {
-	extend: 'Ext.form.Panel'	
+	extend: 'Ext.form.Panel',
+	
+    /* i18n */
+    uploadFileLabel: "Upload File",
+    selectFileButtonText: "Select File:",
+    waitingMessageText: "Waiting...",
+    /* ~i18n */
+
+    width: 600,
+    height: 400,
+    
+    initComponent: function() {
+    	
+	    this.items = [{
+				    	items: [{
+					    	xtype: 'panel',
+					        height: 300,
+					        margin: 2
+				        }]
+	    			},
+	    			{
+	    				xtype: 'panel',
+	    				items : this.getInstitutionButtons()
+	    			},
+	    			{
+		                xtype: 'filefield',
+		                name: 'uploadfile',
+		                fieldLabel: this.uploadFileLabel,
+		                msgTarget: 'side',
+		                allowBlank: false,
+		                anchor: '100%',
+		                margin: 2,
+		                buttonText: this.selectFileButtonText
+	    			}],
+	    			
+		this.buttons = [{
+			text: 'Upload',
+		    handler: function() {
+				if(this.isValid()) {
+					this.submit({
+						url: 'dataportal/uploadFile',
+						waitMsg: this.waitingMessageText,
+				        sucess: function(form, action) {
+				            			
+				        },
+				        failure: function(form, action) {
+				            			
+				        }
+				    })
+				} else {
+					// TODO is form is not valid
+				}
+		    }},{
+		    	text: 'Cancel'
+		    }]
+    	this.callParent(arguments);
+    },
+    
+    /**
+     * 
+     */
+	getInstitutionButtons: function() {
+		
+	}
 });
